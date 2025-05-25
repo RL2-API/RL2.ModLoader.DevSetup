b689c76e30a2e9eba7662431f160f768ef646ae1
TacoConKvass <e_frun@o2.pl>
2025-05-25 09:44:19 +0200

    [Update] Create a .targets file instad of copying libraries

--- Program.cs
+++ Program.cs
@@ -1,40 +1,47 @@
 using NativeFileDialogSharp;
 using System;
 using System.IO;
+using System.Linq;
 using System.Threading;
 
 public class Program
 {
-	static string OutputLibPath = AppDomain.CurrentDomain.SetupInformation.ApplicationBase.Replace("RL2.ModLoader.DevSetup\\", "") + "lib\\";
+	static string OutputPath = string.Join('\\', AppDomain.CurrentDomain.SetupInformation.ApplicationBase!.Split("\\").SkipLast(1)); 
 
 	public static void Main(string[] args) {
 		Console.WriteLine("Welcome to the RL2.ModLoader.DevSetup");
-		Console.WriteLine("Choose your games installation directory");
+		Console.WriteLine("Choose your game's .exe file");
 		Thread.Sleep(500);
 
-		DialogResult result = Dialog.FolderPicker();
-		string DataPath = result.Path + "\\Rogue Legacy 2_Data";
+		DialogResult result = Dialog.FileOpen();
+		string directory = result.Path.Replace("\\Rogue Legacy 2.exe", "");
+		string DataPath = directory + "\\Rogue Legacy 2_Data";
 		string ManagedPath = DataPath + "\\Managed";
 
-		if (!Directory.Exists(DataPath) || !Directory.Exists(ManagedPath)) {
+		if (!File.Exists(directory + "\\Rogue Legacy 2.exe")) {
 			Console.WriteLine("The provided path is incorrect");
+			Console.WriteLine("The selected folder should contain the 'Rogue Legacy 2.exe' file");
 			return;
 		}
 
-		if (!Directory.Exists(OutputLibPath)) {
-			Directory.CreateDirectory(OutputLibPath);
-		}
+		File.WriteAllText(OutputPath + "RL2.ModLoader.targets", $"""
+		<Project ToolsVersion="14.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+			<!-- MSBuild variables -->
+			<PropertyGroup>
+				<RL2_Path>{directory}</RL2_Path>
+				<RL2_DataPath>{DataPath}</RL2_DataPathPath>
+				<RL2_LibPath>{DataPath}</RL2_LibPath>
+			</PropertyGroup>
 
-		Console.WriteLine("\nStart copying libs...");
-		DirectoryInfo librariesDirectory = new DirectoryInfo(ManagedPath);
-		foreach (FileInfo fileInfo in librariesDirectory.GetFiles("**.dll")) {
-			if (fileInfo.Name == "RL2.ModLoader.dll" || fileInfo.Name == "RL2.API.dll") {
-				continue;
-			}
-			Console.WriteLine(fileInfo.Name);
-			File.Copy(fileInfo.FullName, OutputLibPath + fileInfo.Name, true);
-		}
-		Console.WriteLine("Press any key to exit...");
-		Console.ReadKey();
+			<!-- Shared references -->
+			<ItemGroup>
+				<Reference Include="{ManagedPath}\*.dll" />
+			</ItemGroup>
+		</Project>
+		""");
+
+		Console.WriteLine($"RL2.ModLoader.targets created in {OutputPath}");
+		Console.WriteLine("Closing in 2 seconds...");
+		Thread.Sleep(2_000);
 	}
 }
