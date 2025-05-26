92255e8b785af4ec6b7f8756b1659e9d5897318e
TacoConKvass <e_frun@o2.pl>
2025-05-26 12:21:32 +0200

    [Fix]

--- Program.cs
+++ Program.cs
@@ -14,28 +14,31 @@ public class Program
 		Thread.Sleep(500);
 
 		DialogResult result = Dialog.FileOpen();
+
+		if (!result.Path.EndsWith("Rogue Legacy 2.exe")) {
+			Console.WriteLine("The provided path is incorrect");
+			Console.WriteLine("Please select the 'Rogue Legacy 2.exe' file");
+			return;
+		}
+
 		string directory = result.Path.Replace("\\Rogue Legacy 2.exe", "");
 		string DataPath = directory + "\\Rogue Legacy 2_Data";
 		string ManagedPath = DataPath + "\\Managed";
 
-		if (!File.Exists(directory + "\\Rogue Legacy 2.exe")) {
-			Console.WriteLine("The provided path is incorrect");
-			Console.WriteLine("The selected folder should contain the 'Rogue Legacy 2.exe' file");
-			return;
-		}
 
 		File.WriteAllText(OutputPath + "\\RL2.ModLoader.targets", $"""
 		<Project ToolsVersion="14.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
 			<!-- MSBuild variables -->
 			<PropertyGroup>
-				<RL2_Path>{directory}</RL2_Path>
-				<RL2_DataPath>{DataPath}</RL2_DataPath>
-				<RL2_LibPath>{ManagedPath}</RL2_LibPath>
+				<RL2_Path>{directory}\</RL2_Path>
+				<RL2_DataPath>{DataPath}\</RL2_DataPath>
+				<RL2_LibPath>{ManagedPath}\</RL2_LibPath>
+				<RL2_ModsPath>{DataPath}\Mods\</RL2_ModsPath>
 			</PropertyGroup>
 
 			<!-- Shared references -->
 			<ItemGroup>
-				<Reference Include="{ManagedPath}\*.dll">
+				<Reference Include="$(RL2_LibPath)*.dll">
 					<Private>false</Private>
 				</Reference>
 			</ItemGroup>
